2545107161f58296d537afcf383e19ae32344f8d
manoel jailton <manoeljailton79@gmail.com>
2023-04-04 22:13:20 -0300

    seção sobre

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/manoel-jj.png" />
+    <Titulo fontSize={20}>Manoel jailton</Titulo>
+  </aside>
+)
+
+export default Sidebar
